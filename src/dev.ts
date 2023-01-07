f2a1f42371cf869d35c29b411e1399d92a6883cf
FLzyy <flzyy72@gmail.com>
2023-01-07 17:20:37 +0800

    basic functionality

--- src/dev.ts
+++ src/dev.ts
@@ -1,5 +1,27 @@
-/**
- * Dev file:
- *
- * Use this to manually test `index.ts`
- */
+import Timer from "./index";
+
+const timer = new Timer();
+
+let i = 0;
+while (i < 1000000000) {
+  i++;
+}
+
+timer.end();
+
+console.log(timer.time);
+
+timer.reset();
+
+timer.start();
+
+let h = 0;
+while (h < 1000000000) {
+  h++;
+}
+
+timer.end();
+
+console.log(timer.time);
+
+console.log(timer.stored);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-/**
- * Index:
- *
- * Main entrypoint for your package.
- */
+interface Times {
+  ns: bigint;
+  ms: number;
+  s: number;
+}
+
+export default class Timer {
+  stored: Times[];
+  private sTime: bigint;
+  private eTime: bigint;
+
+  constructor() {
+    this.sTime = process.hrtime.bigint();
+    this.eTime = 0n;
+    this.stored = [];
+  }
+
+  start(): void {
+    if (this.sTime !== 0n) {
+      throw new Error("Timer is still currently ongoing.");
+    }
+    this.sTime = process.hrtime.bigint();
+  }
+
+  end(): void {
+    this.eTime = process.hrtime.bigint();
+  }
+
+  get time(): Times {
+    const ns = this.eTime - this.sTime;
+    const all = {
+      ns,
+      ms: Number(ns / 1000000n),
+      s: Number(ns / 1000000000n),
+    };
+
+    this.stored.push(all);
+
+    return all;
+  }
+
+  reset(): void {
+    this.sTime = 0n;
+    this.eTime = 0n;
+  }
+}
